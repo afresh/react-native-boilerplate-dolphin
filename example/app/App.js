8f48c15e555088704bf92071d1fe3aa49ec40256
优三弟 <luowei@tlhkj.net>
2018-12-14 10:16:23 +0800

    update styles

--- example/app/App.js
+++ example/app/App.js
@@ -4,18 +4,12 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import {Provider} from 'mobx-react';
 
 import * as RootStore from './store/RootStore';
-
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-    android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {Theme} from './util';
 
 export default class App extends Component<any> {
     constructor(props: any) {
@@ -29,31 +23,11 @@ export default class App extends Component<any> {
     render() {
         return (
             <Provider {...RootStore}>
-                <View style={styles.container}>
-                    <Text style={styles.welcome}>Welcome to React Native!</Text>
-                    <Text style={styles.instructions}>To get started, edit App.js</Text>
-                    <Text style={styles.instructions}>{instructions}</Text>
+                <View style={Theme.app}>
+                    <Text>Welcome to React Native!</Text>
+                    <Text>To get started, edit App.js</Text>
                 </View>
             </Provider>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
--- example/app/util/Theme/DefaultTheme.js
+++ example/app/util/Theme/DefaultTheme.js
@@ -3,3 +3,13 @@
  * @author author-Afresh(danding_ge@qq.com)
  */
 
+const defaultTheme = {
+    app: {
+        backgroundColor: '#F5FCFF',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+};
+
+export default defaultTheme;
